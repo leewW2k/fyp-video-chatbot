6efb18e51a38f4ec5e240bd5b2d2b897a2f467b4
leewW2k <killerrobbot@gmail.com>
2024-07-06 01:24:02 +0800

    1: enable history

--- chatbot-service/openai_service.py
+++ chatbot-service/openai_service.py
@@ -6,12 +6,15 @@ import logging
 
 class OpenAIService:
     def __init__(self):
+        self.system_message = "You are a helpful assistant."
         self.azure_endpoint = os.environ.get("YOUR_ENDPOINT_NAME")
         self.api_key = os.environ.get("YOUR_API_KEY")
         self.deployment_name = os.environ.get("YOUR_DEPLOYMENT_NAME")
         self.api_version = os.environ.get("YOUR_API_VERSION")
         self.client = self.initiate_client()
         self.logger = logging.getLogger()
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+        self.messages_array = [{"role": "system", "content": self.system_message}]
 
     def initiate_client(self):
         try:
@@ -36,7 +39,20 @@ class OpenAIService:
         # Print the response
         print("Response: " + generated_text + "\n")
 
-    def create_prompt_response(self):
+    def create_response_history(self, prompt: str):
+        self.messages_array.append({"role": "user", "content": prompt})
+
+        response = self.client.chat.completions.create(
+            model=self.deployment_name,
+            messages=self.messages_array
+        )
+        generated_text = response.choices[0].message.content
+        self.messages_array.append({"role": "assistant", "content": generated_text})
+
+        # Print the response
+        print("Response: " + generated_text + "\n")
+
+    def create_prompt_response(self, history=True):
         while True:
             # Get input text
             input_text = input("Enter the prompt (or type 'quit' to exit): ")
@@ -47,4 +63,9 @@ class OpenAIService:
                 continue
 
             print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
-            self.create_response(input_text)
+            self.logger.info("History Enabled: " + str(history))
+            if history:
+                self.messages_array = [{"role": "system", "content": self.system_message}]
+                self.create_response_history(input_text)
+            else:
+                self.create_response(input_text)
