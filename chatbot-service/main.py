d4f2f64d7916cd5bb41f98bae27507e8ba364594
leewW2k <killerrobbot@gmail.com>
2024-07-05 22:26:58 +0800

    1: working openai response

---
+++ chatbot-service/main.py
@@ -0,0 +1,13 @@
+import openai_service
+import dotenv
+
+dotenv.load_dotenv()
+
+
+def main():
+    client = openai_service.OpenAIService()
+    client.create_response()
+
+
+if __name__ == "__main__":
+    main()
